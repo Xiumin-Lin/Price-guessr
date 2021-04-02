1b64355ac4dc47655d0508a84c9324f8fc96168a
Xiumin Lin <xiumin.lin@laposte.net>
2021-04-02 16:30:14 +0200

    Set Session storage

--- main.js
+++ main.js
@@ -7,11 +7,10 @@ const itemName = document.getElementById("name");
 const maxScorePerQuestion = 100;
 const max_aaa_page = 25;
 const max_attempt = 3;
-const max_page = 50;
+const max_page = 50; //if filter for aaa is disable
 const nbGame = document.getElementById("nbGame");
 const urlPrice =  "https://www.cheapshark.com/api/1.0/deals";
 const user_price = document.getElementById("user_price");
-
 let correctPrice = 0;
 let user_attempt = 0;
 
@@ -19,6 +18,7 @@ function play(){
     console.log("Start game !") //TODO
     document.getElementById("play").hidden = true;
     page = getRandomNumber(0, max_aaa_page);
+
     console.log("page", page); //TODO
     // fetch
     fetch(`${urlPrice}?storeID=1&pageNumber=${page}&AAA=1`)
@@ -29,6 +29,8 @@ function play(){
         console.log("Data :", data); //TODO
         loadData(data);
     })
+    sessionStorage.setItem("final_score", final_score.innerText);
+    sessionStorage.setItem("nbGame", nbGame.innerText);
 
     // const urlSteam =  "https://store.steampowered.com/api/appdetails"
     // let gameSteamID; //637090
@@ -56,9 +58,14 @@ function loadData(data){
     let item = getRandomItem(data);
     console.log(item) //TODO
     // document.getElementById("res").innerText = JSON.stringify(item, null, 4); //TODO
+    setQuiz(item);
+    setSessionGameItem(item);
+}
+
+function setQuiz(item){
     itemName.innerText = item["title"];
     itemImg.src = item["thumb"];
-    correctPrice = item["salePrice"];
+    correctPrice = Math.round(parseFloat(item["salePrice"]));
     box.hidden = false;
 }
 
@@ -67,12 +74,18 @@ function checkPrice(price) {
 }
 
 function getQuestionScore() {
-    return Math.floor((maxScorePerQuestion - user_attempt*10) - Math.abs(parseInt(correctPrice) - user_price.valueAsNumber));
+    let score = Math.round((maxScorePerQuestion - user_attempt*10) - Math.abs(correctPrice - user_price.valueAsNumber));
+    return (score >= 0)? score : 0;
 }
 
 function getAttemptsLeft() {
     return (max_attempt - user_attempt);
 }
+
+document.getElementById("guess").addEventListener("submit", (e)=>{
+    e.preventDefault();
+    guess();
+});
 
 function guess(){
     if(user_price.value === "") {
@@ -102,9 +115,34 @@ function guess(){
         answer.innerText = `Correct answer is ${correctPrice} ! You won ${score}/${maxScorePerQuestion} point(s)`;
         nbGame.innerText = parseInt(nbGame.innerText) + 1;
         final_score.innerText = parseInt(final_score.innerText) + score;
+        console.log("score",score);
+        console.log("total",final_score.innerText);
         user_attempt = 0;
         attempts.innerText = '';
         play();
     }
     user_price.value = "";
+    sessionStorage.setItem("user_attempt", user_attempt);
+}
+
+if(sessionStorage.length){
+    if(confirm("Load last game session ?"))
+        setLastSession();
+    else
+        sessionStorage.clear();
+}
+
+function setSessionGameItem(item){
+    let jsonItem = JSON.stringify(item);
+    sessionStorage.setItem("game", jsonItem);
+}
+
+function setLastSession(){
+    console.log(sessionStorage); //TODO
+    document.getElementById("play").hidden = true;
+    setQuiz(JSON.parse(sessionStorage.getItem("game")));
+
+    user_attempt = sessionStorage.getItem("user_attempt");
+    final_score.innerText = sessionStorage.getItem("final_score");
+    nbGame.innerText = sessionStorage.getItem("nbGame");
 }
